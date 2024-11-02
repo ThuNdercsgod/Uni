8bac310de34280ff964cdde516f5d81457a52903
ThuNdercsgod <georgi.naoumov@gmail.com>
2024-11-02 18:29:38 +0200

    Pract. 22-10-2024

---
+++ C/Course 1/Semester 1/Markovski/22-10-2024.c
@@ -0,0 +1,143 @@
+#include <stdio.h>
+
+int main()
+{
+    // //zadacha 1
+    // int N;
+    // int Pn = 1;
+
+    // printf("Enter a number for the permutation:\n");
+    // scanf("%d", &N);
+
+    // for (int i = 1; i <= N; i++)
+    // {
+    //     Pn = Pn * i;
+    // }
+
+    // printf("The permutation P(%d) = %d\n", N, Pn);
+
+    // //zadacha 2
+    // int n, m;
+    // char l;
+
+    // printf("Enter and interval of 2 numbers in the interval [0; 255]:\n");
+    // scanf("%d %d", &n, &m);
+
+    // if ((n <= 255 && n >= 0) && (m <= 255 && m >= 0))
+    // {
+    //     for (int i = n; i<=m; i++)
+    //     {
+    //         l = i;
+    //         printf("%d -> %c\n", i, l);
+    //     }
+    // }
+    // else
+    // {
+    //     printf("Invalid data!\n");
+    // }
+
+    // //zadacha 3
+    // int x, y;
+    // int n;
+
+    // printf("Enter the value of \"n\" in the equation \"x+y=n\":\n");
+    // scanf("%d %d %d", &x, &y, &n);
+
+    // for (int i = 0; i <= n; i++)
+    // {
+    //     x = i;
+    //     y = n - x;
+
+    //     printf("x = %d, y = %d\n", x, y);
+    // }
+
+    // //zadacha 4
+    // int N = 0;
+
+    // printf("Enter the value of\"N\":\n");
+    // scanf("%d", &N);
+
+    // while (N != 1)
+    // {
+    //     if (N % 2 == 0)
+    //     {
+    //         N = N / 2;
+    //         printf("%d\n", N);
+    //     }
+    //     else
+    //     {
+    //         N = 3 * N + 1;
+    //         printf("%d\n", N);
+    //     } 
+    // }
+    
+    // //zadacha 5
+    // int num;
+
+    // printf("Enter a positive number:\n");
+    // scanf("%d", &num);
+
+    // if (num >= 0)
+    // {
+    //     pow(num, 2);
+
+    // }
+
+    // //zadacha 6
+    // int m, n;
+
+    // printf("Enter the value of \"m\" and \"n\"\n");
+    // scanf("%d %d", &m, &n);
+
+    // int x = m * n;
+
+    // for (int i = 1; i <= m; i++)
+    // {
+    //     for (int j = 1; j <= n; j++)
+    //     {
+    //         printf("%d ", j);
+    //     }
+    //     printf("\n");
+    // }
+
+    //zadacha 7
+
+    // //zadacha 9
+    // int N, M;
+
+    // printf("Enter the interval:\n");
+    // scanf("%d %d", &N, &M);
+
+    // for (int i = N; i <= M; i++)
+    // {
+
+    // }
+
+    //zadacha 11
+    int num;
+
+    printf("%d", num);
+
+    // //zadacha 20
+    // int n;
+    // int rows = n * 2 - 1;
+    // int space = n * 2 + 1;
+    // int number = 1;
+
+    // //print ot 1 do number
+    
+    // //1->space
+    // for (int i = 1; i <= number; i++)
+    // {
+    //     for (int j = 1; j <= space; j++)
+    //     {
+    //         for (int k = number; k <= 1; k ++)
+    //         {
+    //             number++;
+    //             space += 2;
+    //         }
+    //     }
+    // }
+
+    return 0;
+}
