1a811a0b3ea005b9d40b00368da7944b9065f937
ThuNdercsgod <georgi.naoumov@gmail.com>
2024-11-29 23:20:36 +0200

    Упражнение (5) v3.0

--- C/Course 1/Semester 1/Armqnov/Упражнение/Задачи за упражнение (5)/upr5.c
+++ C/Course 1/Semester 1/Armqnov/Упражнение/Задачи за упражнение (5)/upr5.c
@@ -6,13 +6,14 @@
 #define EPS 1E-9
 
 void transform(double arr[], unsigned  size);
-void checkNext(double arr[], unsigned size);
-double removeNext(double arr[], unsigned size);
-void isSet(double arr[], unsigned size);
-double toSet(double arr[], unsigned size);
-double unionOfArrays(double arr1[], double arr2[], double arrUnion[], unsigned size);
-//с union ми дава грешка, затова сложих друго име
-double sortArray(double arr[], unsigned size);
+int checkNext(double arr[], unsigned size);
+void removeNext(double arr[], unsigned size);
+int isSet(double arr[], unsigned size);
+void toSet(double arr[], unsigned size);
+void unionOfArrays(double arr1[], double arr2[], double arrUnion[], unsigned size);
+// с union ми дава грешка, затова сложих друго име
+void intersection(double arr1[], double arr2[], double arrIntersection[], unsigned size);
+void sortArray(double arr[], unsigned size);
 void printArray(double arr[], unsigned size);
 
 int main()
@@ -35,36 +36,64 @@ int main()
     printArray(check, CAPACITY);
     puts("");
 
-    puts("Checking if there are the the same elements next to eachother");
-    puts("...");
-    checkNext(check, CAPACITY);
-    puts("\nare the elements that exist more than once.");
+    (checkNext(check, CAPACITY) == 1)? puts("There are repeating elements next to eachother") : 
+    puts("There are no repeating elements next to eachother");
     puts("");
-    
+ 
     removeNext(check, CAPACITY);
 
     puts("When removing the same elements next to eachother, the array is:");
     printArray(check, CAPACITY);
     puts("");
 
-    double set[CAPACITY] = { 0, 1, 2, 1, 3, 4, 1, 0, 2, 0 };
+    double set[CAPACITY] = { 0, 1, 2, 1, 3, 4, 1, 0, 0, 2, 0 };
 
     puts("The array is:");
     printArray(set, CAPACITY);
     puts("");
 
-    isSet(set, CAPACITY);
-    puts("");
+    if (isSet(set, CAPACITY) == 0)
+    {
+        puts("The array is not a set");
+        puts("");
 
-    toSet(set, CAPACITY);
-    sortArray(set, CAPACITY);
+        toSet(set, CAPACITY);
+        sortArray(set, CAPACITY);
 
-    puts("When remade into a set, the array is:");
+        puts("When remade into a set, the array is:");
+        printArray(set, CAPACITY);
+        puts("");
+    }
+    else
+    {
+        puts("The array is a set\n");
+    }
+
+    double arrUnion[BIGCAPACITY] = { 0 };
+
+    unionOfArrays(check, set, arrUnion, BIGCAPACITY);
+
+    puts("The Union of the arrays");
+    printArray(check, CAPACITY);
+    puts("and");
     printArray(set, CAPACITY);
+    puts("is:");
+    
+    printArray(arrUnion, BIGCAPACITY);
     puts("");
 
-    //double arrUnion[BIGCAPACITY] = { 0 };
-    //unionOfArrays(check, set, arrUnion, BIGCAPACITY);
+    double arrIntersection[BIGCAPACITY] = { 0 };
+
+    intersection(check, set, arrIntersection, BIGCAPACITY);
+
+    puts("The intersection of the arrays");
+    printArray(check, CAPACITY);
+    puts("and");
+    printArray(set, CAPACITY);
+    puts("is:");
+    
+    printArray(arrIntersection, BIGCAPACITY);
+    puts("");
 
     return 0;
 }
@@ -88,29 +117,32 @@ void transform(double arr[], unsigned size)
     }
 }
 
-void checkNext(double arr[], unsigned size)
+int checkNext(double arr[], unsigned size)
 {
-    for (int i = 0; i < (size); ++i)
+    for (int i = 1; i < (size); ++i)
     {
         if(fabs(arr[i] - arr[i - 1]) < EPS)
         {
-            printf("arr[%d] = arr[%d] = %lf ", i - 1, i, arr[i]);
+            return 1;
         }
     }
+
+    return 0;
 }
 
-double removeNext(double arr[], unsigned size)
+void removeNext(double arr[], unsigned size)
 {
-    for (int i = 0; i < (size); ++i)
+    for (int i = 1; i < (size); ++i)
     {
         if(fabs(arr[i] - arr[i - 1]) < EPS)
         {
             arr[i - 1] = 0;
         }
     }
+    sortArray(arr, size);
 }
 
-void isSet(double arr[], unsigned size)
+int isSet(double arr[], unsigned size)
 {
     for (int i = 0; i < (size - 1); ++i)
     {
@@ -118,15 +150,14 @@ void isSet(double arr[], unsigned size)
         {
             if(fabs(arr[i] - arr[j]) < EPS && i != j)
             {
-                puts("The array is not a set");
-
-                return;
+                return 0;
             }
         }
     }
+    return 1;
 }
 
-double toSet(double arr[], unsigned size)
+void toSet(double arr[], unsigned size)
 {
     for (int i = 0; i < (size - 1); ++i)
     {
@@ -138,23 +169,74 @@ double toSet(double arr[], unsigned size)
             }
         }
     }
-}
-//todo finish it after the sorting algorithm
-double unionOfArrays(double arr1[], double arr2[], double arrUnion[], unsigned size)
-{
-    
+    sortArray(arr, size);
 }
 
-//TODO fix it, cuz something wrong
-double sortArray(double arr[], unsigned size)
+void unionOfArrays(double arr1[], double arr2[], double arrUnion[], unsigned size)
 {
-    for (int i = 1; i < size; ++i)
+    int size1 = 0, size2 = 0;
+
+    while (!(fabs(arr1[size1]) < EPS))
     {
-        if (fabs(arr[i - 1]) < EPS)
+        size1++;
+    }
+    while (!(fabs(arr2[size2]) < EPS))
+    {
+        size2++;
+    }
+
+    for (int i = 0; i <= size1; ++i)
+    {
+        arrUnion[i] = arr1[i];
+    }
+
+    for (int i = size1; i <= size1 + size2; ++i)
+    {
+        arrUnion[i] = arr2[i - size1];
+    }
+
+    toSet(arrUnion, size);
+}
+
+void intersection(double arr1[], double arr2[], double arrIntersection[], unsigned size)
+{
+    int size1 = 0, size2 = 0;
+
+    while (!(fabs(arr1[size1]) < EPS))
+    {
+        size1++;
+    }
+    while (!(fabs(arr2[size2]) < EPS))
+    {
+        size2++;
+    }
+
+    for (int i = 0; i <= size1; ++i)
+    {
+        for (int j = 0; j <= size2; ++j)
         {
-            arr[i - 1] = arr[i];
-            arr[i] = 0;
+            if (fabs(arr1[i] - arr2[j]) < EPS)
+            {
+                arrIntersection[i] = arr1[i];
+            }
         }
+    }
+    sortArray(arrIntersection, size);
+}
+
+void sortArray(double arr[], unsigned size)
+{
+    for (int i = 0; i < size; ++i)
+    {
+        int j = 0;
+        while (!(fabs(arr[j]) < EPS))
+        {
+            j++;
+        }
+
+        if (!(fabs(arr[i]) < EPS))
+        arr[j] = arr[i];
+        arr[i] = 0;
     }
 }
 
