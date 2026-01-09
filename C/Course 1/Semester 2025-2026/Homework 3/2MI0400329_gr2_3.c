fe199736d46ec78b5f49a83630eedecec68433e6
ThuNdercsgod <georgi.naoumov@gmail.com>
2026-01-09 17:37:01 +0200

    Homework 3 Task 3 v2.0
    Final

--- C/Course 1/Semester 2025-2026/Homework 3/2MI0400329_gr2_3.c
+++ C/Course 1/Semester 2025-2026/Homework 3/2MI0400329_gr2_3.c
@@ -14,6 +14,7 @@ void printTable(char **table, int words);
 int wordCount(char *sentence);
 void addWordsToTable(char *sentence, char **table, int words);
 int wordRepeats(char *word, char **table, int words);
+int removeRepeats(char *word, char **table, int *wordReps, int *words);
 int rearrangeTable(char **table, int *words);
 
 void compress(char *sentence, char **table, int words, char *compressed);
@@ -51,6 +52,13 @@ int main()
     compress(sentence, table, words, compressed);
     puts(compressed);
 
+    for (int i = 0; i < words; ++i)
+    {
+        printf(table[i]);
+        printf(" ");
+    }
+    printf("\n");
+
     char decompressed[CAP];
     decompress(compressed, table, words, decompressed);
     puts(decompressed);
@@ -65,17 +73,21 @@ int stringcmp(char *first, char *second)
     int lengthFirst = strlen(first);
     int lengthSecond = strlen(second);
 
-    if (lengthFirst != lengthSecond)
-    {
-        return lengthFirst - lengthSecond;
-    }
-
     for (int i = 0; i < lengthFirst; ++i)
     {
-        if (first[i] != second[i])
+        if (first[i] > second[i])
         {
             return 1;
         }
+        else if (first[i] < second[i])
+        {
+            return -1;
+        }
+    }
+
+    if (lengthFirst != lengthSecond)
+    {
+        return lengthFirst - lengthSecond;
     }
 
     return 0;
@@ -195,7 +207,7 @@ int wordRepeats(char *word, char **table, int words)
     return count;
 }
 
-int removeRepeats(char *word, char **table, int *words)
+int removeRepeats(char *word, char **table, int *wordReps, int *words)
 {
     int hasAppeared = 0;
     for (int i = 0; i < *words; ++i)
@@ -208,6 +220,8 @@ int removeRepeats(char *word, char **table, int *words)
             for (int j = i; j < (*words - 1); ++j)
             {
                 table[j] = table[j + 1];
+                wordReps[j] = wordReps[j + 1];
+                wordReps[j + 1] = 0;
             }
             table[*words - 1] = NULL;
             *words = *words - 1;
@@ -218,6 +232,13 @@ int removeRepeats(char *word, char **table, int *words)
                 return -1;
             }
             table = newTable;
+
+            int *newReps = (int *)realloc(wordReps, (*words) * sizeof(int));
+            if (!newReps)
+            {
+                return -1;
+            }
+            wordReps = newReps;
             i--;
         }
         else if (stringcmp(word, table[i]) == 0 &&
@@ -231,23 +252,66 @@ int removeRepeats(char *word, char **table, int *words)
 
 int rearrangeTable(char **table, int *words)
 {
+    int *wordReps = (int *)malloc((*words) * sizeof(int));
+    if (!wordReps)
+    {
+        return -1;
+    }
+
+    for (int i = 0; i < *words; ++i)
+    {
+        wordReps[i] = wordRepeats(table[i], table, *words);
+    }
+
     for (int i = 0; i < *words; ++i)
     {
         if (wordRepeats(table[i], table, *words) > 1)
         {
-            if (removeRepeats(table[i], table, words) == -1)
+            if (removeRepeats(table[i], table, wordReps, words) == -1)
             {
+                free(wordReps);
+                wordReps = NULL;
                 return -1;
             }
-            i--; // Adjust index after removal
+            i--;
         }
     }
+
+    for (int i = 0; i < *words - 1; ++i)
+    {
+        for (int j = 0; j < *words - i - 1; ++j)
+        {
+            if (wordReps[j] < wordReps[j + 1])
+            {
+                int temp = wordReps[j + 1];
+                wordReps[j + 1] = wordReps[j];
+                wordReps[j] = temp;
+
+                char *tempWord = table[j + 1];
+                table[j + 1] = table[j];
+                table[j] = tempWord;
+            }
+            else if (wordReps[j] == wordReps[j + 1] &&
+                     stringcmp(table[j], table[j + 1]) > 0)
+            {
+                int temp = wordReps[j + 1];
+                wordReps[j + 1] = wordReps[j];
+                wordReps[j] = temp;
+
+                char *tempWord = table[j + 1];
+                table[j + 1] = table[j];
+                table[j] = tempWord;
+            }
+        }
+    }
+
+    free(wordReps);
+    wordReps = NULL;
+
     return 0;
 }
 
 void compress(char *sentence, char **table, int words, char *compressed)
-// наредбата им е както е редът в изречението, не успях да го направя да се подреждат
-// по честота
 {
     if (words < 10)
     {
